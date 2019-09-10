498bf85d4af48bd7ab16d77b3754ba427247656d
x1ah <x1ahgxq@gmail.com>
2019-09-10 17:09:18 +0800

    bump stable version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with io.open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='outplan',
 
-    version='0.1.3',
+    version='1.0.0',
 
     description='Support nested experiment/namespace base on Facebook Planout',
     long_description=long_description,
