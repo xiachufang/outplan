1b150f5e30eaca6e1d5f45d38a88e624df57fb63
straydragon <straydragonl@foxmail.com>
2024-04-23 16:51:51 +0800

    feat!: 放弃2.7支持&使用更新工具链和配置

--- outplan/__init__.py
+++ outplan/__init__.py
@@ -1,9 +1,8 @@
-# coding: utf-8
-
 from .client import ExperimentGroupClient
 from .exceptions import ExperimentBaseError
 from .experiment import ExperimentItem, GroupItem, NamespaceItem
 
+__version__ = "3.0.0"
 
 __all__ = (
     "ExperimentGroupClient",
