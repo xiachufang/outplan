a6f1eb076d0261ea63274c14860991b9378e0e87
straydragon <straydragonl@foxmail.com>
2024-04-23 18:53:26 +0800

    fix: 避免与自动release冲突

--- outplan/__init__.py
+++ outplan/__init__.py
@@ -2,7 +2,7 @@ from .client import ExperimentGroupClient
 from .exceptions import ExperimentBaseError
 from .experiment import ExperimentItem, GroupItem, NamespaceItem
 
-__version__ = "3.0.0"
+__version__ = "2.2.0"
 
 __all__ = (
     "ExperimentGroupClient",
